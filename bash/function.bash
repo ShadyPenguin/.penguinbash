d83102b1e276f8e1828d64df8c4dba856a20258b
ShadyPenguin <jakesikora.01@gmail.com>
2020-04-09 13:47:55 -0500

    added commits beforehand

--- bash/function.bash
+++ bash/function.bash
@@ -1,5 +1,7 @@
 # Penguin functions
 remote_diff () {
+	git outs
+	read -n 1 -p "Press any key to proceed to diff" discard
 	branch_name=$(git rev-parse --abbrev-ref HEAD)
 	git diff origin/"$branch_name"..HEAD
 }
