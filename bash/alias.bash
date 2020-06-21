52f5e9d6e1b9ea41208459019cbc9be7a79c24f8
ShadyPenguin <jakesikora.01@gmail.com>
2020-06-21 14:45:26 -0500

    updating local git config

--- bash/alias.bash
+++ bash/alias.bash
@@ -8,9 +8,10 @@ alias ll="ls -al"
 alias gcm="git checkout master"
 alias grr="git reset --hard head"
 alias vi="vim"
+alias kc="kubectl"
 
 #################################
 # Penguin related aliases
 #################################
-alias rs=". ${PENGUIN_HOME}/bash/bashrc.bash"
+alias rs=". ${PENGUIN_HOME}bash/bashrc.bash"
 alias home="cd ${PENGUIN_HOME}"
